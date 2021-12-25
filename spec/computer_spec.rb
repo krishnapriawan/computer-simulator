4f2e587b55d7ccdb1c52fc575837c5c020f8d400
krishnapriawan <krishna.priawan@gmail.com>
2021-12-25 14:14:13 +0900

    Add unit tests
    - .set_address

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -16,4 +16,32 @@ describe Computer do
     end
   end
 
+  context '.set_address' do
+    computer = Computer.new(1000)
+
+    it 'should succeed' do
+      expect(computer.set_address(2)).to eql(computer)
+    end
+
+    it 'should succeed at last index of stack' do
+      expect(computer.set_address(999)).to eql(computer)
+    end
+
+    it 'should raise an error for out of bound address' do
+      expect { computer.set_address(1000) }.to raise_error StackOutOfBoundError
+    end
+
+    it 'should raise an error for negative number' do
+      expect { computer.set_address(-2) }.to raise_error InvalidNumberError
+    end
+
+    it 'should raise an error for string' do
+      expect { computer.set_address('2') }.to raise_error InvalidValueError
+    end
+
+    it 'should raise an error for nil' do
+      expect { computer.set_address(nil) }.to raise_error InvalidValueError
+    end
+  end
+
 end
