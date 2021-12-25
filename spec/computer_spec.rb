7aead334544e9680e53ade2eb9dec9fc69c4f6cb
krishnapriawan <krishna.priawan@gmail.com>
2021-12-25 14:14:38 +0900

    Add unit tests
    - .insert
    - .execute

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -44,4 +44,59 @@ describe Computer do
     end
   end
 
+  context '.insert' do
+    computer = Computer.new(1000)
+
+    it 'should insert valid instructions' do
+      expect(computer.set_address(0).insert('PUSH', 5).insert('PRINT').insert('CALL', 45)).to eql(computer)
+      expect(computer.set_address(10).insert('PUSH', 10).insert('PUSH', 9).insert('MULT')).to eql(computer)
+      expect(computer.set_address(10).insert('RET', 10).insert('PUSH', 9).insert('STOP')).to eql(computer)
+    end
+
+    it 'should raise an error for unknown instructions' do
+      expect { computer.set_address(0).insert('ADD').insert('PRINT') }.to raise_error InvalidValueError
+    end
+
+    it 'should raise an error for inserting PUSH without value parameter' do
+      expect { computer.set_address(0).insert('PUSH').insert('PRINT') }.to raise_error InvalidValueError
+    end
+
+    it 'should raise an error for inserting PUSH with non-numeric value' do
+      expect { computer.set_address(0).insert('PUSH', '5').insert('PRINT') }.to raise_error InvalidValueError
+    end
+
+    it 'should raise an error for inserting CALL without value parameter' do
+      expect { computer.set_address(0).insert('CALL').insert('PRINT') }.to raise_error InvalidValueError
+    end
+
+    it 'should raise an error for inserting CALL with non-numeric value' do
+      expect { computer.set_address(0).insert('CALL', '5').insert('PRINT') }.to raise_error InvalidValueError
+    end
+
+    it 'should raise an error for inserting at out of bound address' do
+      expect { computer.set_address(999).insert('PUSH', 999).insert('PUSH', 1000) }.to raise_error InvalidValueError
+    end
+  end
+
+  context '.execute' do
+    PRINT_TENTEN_BEGIN = 50
+    MAIN_BEGIN = 0
+    it 'should execute sample test' do
+      computer = Computer.new(100)
+
+      computer.set_address(PRINT_TENTEN_BEGIN).insert('MULT').insert('PRINT').insert('RET')
+      computer.set_address(MAIN_BEGIN).insert('PUSH', 1009).insert('PRINT')
+      computer.insert('PUSH', 6)
+      computer.insert('PUSH', 101).insert('PUSH', 10).insert('CALL', PRINT_TENTEN_BEGIN)
+      computer.insert('STOP')
+      expect { computer.set_address(MAIN_BEGIN).execute }.to output("1009\n1010\n").to_stdout
+    end
+
+    it 'should raise an error when data stack is empty' do
+      computer = Computer.new(3)
+
+      computer.set_address(0).insert('PUSH', 10).insert('PRINT').insert('PRINT')
+      expect { computer.set_address(0).execute }.to raise_error EmptyDataStack
+    end
+  end
 end
