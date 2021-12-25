0718e0b7762ae86972cd9c8338bd6a4918b22b1d
krishnapriawan <krishna.priawan@gmail.com>
2021-12-25 14:12:02 +0900

    Add custom exception classes

---
+++ lib/exceptions.rb
@@ -0,0 +1,4 @@
+class InvalidValueError < StandardError; end
+class InvalidNumberError < StandardError; end
+class StackOutOfBoundError < StandardError; end
+class EmptyDataStack < RuntimeError; end
